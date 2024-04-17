3e71f1cd50648859450091d3517848f70ae26a7f
Galex <43304388+GalexDL@users.noreply.github.com>
2024-04-17 16:27:21 -0400

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -2,4 +2,5 @@ var ROMLIST = [
     {url:"roms/LoZMajorasMask.z64",title:"Majora Mask"},
     {url:"roms/HarvestMoon64.v64",title:"Harvest Moon"},
     {url:"roms/AidynChronicles.v64",title:"Aidyn Chronicles"},
+    {url:"https://upnow-prod.ff45e40d1a1c8f7e7de4e976d0c9e555.r2.cloudflarestorage.com/ZG0THBcVynSYa0NLnmeWgUpqcFf1/ddb07884-d021-4088-8f94-1311299b2bef?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=cdd12e35bbd220303957dc5603a4cc8e%2F20240417%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20240417T202454Z&X-Amz-Expires=43200&X-Amz-Signature=a3381499fcd3dc34b5184d57a7497b95e56a4058230d1a6bb4a935a6467ec09c&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22LoZMajorasMask.z64",title:"Majora Mask"},
 ];
