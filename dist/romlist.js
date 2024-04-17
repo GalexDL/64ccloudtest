d59c29a5e5e6c07afe6356f43646ac73b1cbda19
Galex <43304388+GalexDL@users.noreply.github.com>
2024-04-17 14:39:41 -0400

    U2

---
+++ dist/romlist.js
@@ -0,0 +1,5 @@
+var ROMLIST = [
+    {url:"roms/LoZMajorasMask.z64",title:"Majora Mask"},
+    {url:"roms/HarvestMoon64.v64",title:"Harvest Moon"},
+    {url:"roms/AidynChronicles.v64",title:"Aidyn Chronicles"},
+];
