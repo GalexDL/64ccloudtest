a2bef73ce745bfd91c1ef4d60db6c23d40d03cf4
Galex <43304388+GalexDL@users.noreply.github.com>
2024-07-24 19:24:35 -0400

    v0.1

--- WFSE.py
+++ WFSE.py
@@ -18,7 +18,7 @@ def update_item_list(filename, item_ids, item_names):
                         value = int(value)
                     item_list[str(item_id)] = value
                 except ValueError:
-                    print(f"Invalid input for item {item_id if item_name is None else item_name}")  # Improved error message
+                    print(f"Invalid input for item {item_id if item_name is None else item_name}")  
 
             f.seek(0)
             json_data = json.dumps(data, indent=2)
@@ -32,23 +32,23 @@ def update_item_list(filename, item_ids, item_names):
 def get_values():
 
     item_values = [entry.get() for entry in entry_values]
-    update_item_list("save.json", item_ids, item_names)  # Pass item_names if available
-    window.destroy()  # Close window after processing
+    update_item_list("save.json", item_ids, item_names)  
+    window.destroy()  
 
 
-# Item IDs and optional item names (modify as needed)
+# Item IDs 
 item_ids = (999000, 999001, 999003, 999004, 999005, 999002, 999006)
 item_names = ("Premium Ticket", "1-Pull Summoning Ticket", "10-Pull Summoning Ticket", "1-Pull Armament Ticket", "1-Pull Armament Ticket", "Dream Ticket Summon", "Dream Ticket Armament",)  # Example
 
-# Create the main window
+# Main Window
 window = tk.Tk()
 window.title("WF Save Editor")
 
-# Create labels and entry fields for each item
+
 item_labels = []
 entry_values = []
 for i, item_id in enumerate(item_ids):
-    item_name = item_names[i] if len(item_names) > i else f"Item {item_id}"  # Use item name or default
+    item_name = item_names[i] if len(item_names) > i else f"Item {item_id}" 
     item_label = tk.Label(window, text=f"Amount of {item_name}:")
     item_label.pack()
 
@@ -57,7 +57,7 @@ for i, item_id in enumerate(item_ids):
     item_labels.append(item_label)
     entry_values.append(entry)
 
-# Create a button to submit the values
+# Save
 submit_button = tk.Button(window, text="Save", command=get_values)
 submit_button.pack()
 
